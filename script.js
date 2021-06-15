5dd656e265011cc5ea3edf25efcd56118f73ca23
krun-krit <thanakit_lue@cmu.ac.th>
2021-06-15 21:39:10 +0700

    test

--- script.js
+++ script.js
@@ -59,59 +59,59 @@
     student2.gender = 'หญิง'
 
 
-     document.getElementById('output').innerText = student;
+    //  document.getElementById('output').innerText = student;
 
-    function addStudentData(student){
-        const output = document.getElementById('output')
-        let row = document.createElement('div')
-        row.classList.add('row')
-        let columnName = document.createElement('div')
-        columnName.classList.add("col-1")
-        columnName.classList.add("offset-1")
-        columnName.innerHTML = 'ชื่อ'
-        let columnValue = document.createElement('div')
-        columnValue = document.createElement('row')
-        columnValue.classList.add('col')
-        columnValue.innerHTML = student.name;
-        row.appendChild(columnName)
-        row.appendChild(columnValue)
-        output.appendChild(row)
-
-
-        row = document.createElement('div')
-        row.classList.add('row')
-        columnName = document.createElement('div')
-        columnName.classList.add("col-1")
-        columnName.classList.add("offset-1")
-        columnName.innerHTML = 'รหัส'
-        columnValue = document.createElement('div')
-        columnValue = document.createElement('row')
-        columnValue.classList.add('col')
-        columnValue.innerHTML = student.username;
-        row.appendChild(columnName)
-        row.appendChild(columnValue)
-        output.appendChild(row)
+    // function addStudentData(student){
+    //     const output = document.getElementById('output')
+    //     let row = document.createElement('div')
+    //     row.classList.add('row')
+    //     let columnName = document.createElement('div')
+    //     columnName.classList.add("col-1")
+    //     columnName.classList.add("offset-1")
+    //     columnName.innerHTML = 'ชื่อ'
+    //     let columnValue = document.createElement('div')
+    //     columnValue = document.createElement('row')
+    //     columnValue.classList.add('col')
+    //     columnValue.innerHTML = student.name;
+    //     row.appendChild(columnName)
+    //     row.appendChild(columnValue)
+    //     output.appendChild(row)
 
 
-        row = document.createElement('div')
-        row.classList.add('row')
-        columnName = document.createElement('div')
-        columnName.classList.add("col-1")
-        columnName.classList.add("offset-1")
-        columnName.innerHTML = 'เพศ'
-        columnValue = document.createElement('div')
-        columnValue = document.createElement('row')
-        columnValue.classList.add('col')
-        columnValue.innerHTML = student.gender;
-        row.appendChild(columnName)
-        row.appendChild(columnValue)
-        output.appendChild(row)
-    }
+    //     row = document.createElement('div')
+    //     row.classList.add('row')
+    //     columnName = document.createElement('div')
+    //     columnName.classList.add("col-1")
+    //     columnName.classList.add("offset-1")
+    //     columnName.innerHTML = 'รหัส'
+    //     columnValue = document.createElement('div')
+    //     columnValue = document.createElement('row')
+    //     columnValue.classList.add('col')
+    //     columnValue.innerHTML = student.username;
+    //     row.appendChild(columnName)
+    //     row.appendChild(columnValue)
+    //     output.appendChild(row)
 
-    window.addEventListener("load",function(){
-            addStudentData(student)
-            addStudentData(student2)
-    })
+
+    //     row = document.createElement('div')
+    //     row.classList.add('row')
+    //     columnName = document.createElement('div')
+    //     columnName.classList.add("col-1")
+    //     columnName.classList.add("offset-1")
+    //     columnName.innerHTML = 'เพศ'
+    //     columnValue = document.createElement('div')
+    //     columnValue = document.createElement('row')
+    //     columnValue.classList.add('col')
+    //     columnValue.innerHTML = student.gender;
+    //     row.appendChild(columnName)
+    //     row.appendChild(columnValue)
+    //     output.appendChild(row)
+    // }
+
+    // window.addEventListener("load",function(){
+    //         addStudentData(student)
+    //         addStudentData(student2)
+    // })
 
     var students = [
         stuednt,
@@ -141,7 +141,12 @@
         row.appendChild(cell)
         tableBody.appendChild(row)
     }
+
+    function addStudentList(studentList){
+        let counter = 1
+        for (student of studentList){
+            addStudentToTable(counter++, student)
+        }
+    }
     
-    window.addEventListener("load", function() {
-        addStudentData(student)
-    })
+  
